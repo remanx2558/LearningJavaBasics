0f36a40df864f29219992335e400393cfd606763
Yashwant Gahlot <ygahlot@newrelic.com>
2025-03-28 04:51:48 +0530

    pg

---
+++ src/PaymentGateway/PaymentGateway.java
@@ -0,0 +1,13 @@
+package PaymentGateway;
+
+public class PaymentGateway {
+    UserController userController;
+    InstrumentController instrumentController;
+    TransactionService transactionService;
+
+    PaymentGateway(){
+        userController=new UserController();
+        instrumentController=new InstrumentController();
+        transactionService=new TransactionService();
+    }
+}
--- src/PaymentGateway/Processor.java
+++ src/PaymentGateway/Processor.java
@@ -1,7 +1,6 @@
 package PaymentGateway;
 
 public class Processor {
-
     void processPayment(TransactionDo transaction){
         //some bussiness logic
 
