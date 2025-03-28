b89dc74c495c4677159ac9377059662ddaa9f0c0
Yashwant Gahlot <ygahlot@newrelic.com>
2025-03-28 06:49:53 +0530

    pg

--- src/PaymentGateway/InstrumentService.java
+++ src/PaymentGateway/InstrumentService.java
@@ -1,11 +1,16 @@
 package PaymentGateway;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public abstract class InstrumentService {
     Map<String, List<Instrument>> userInstruments;
+
+    InstrumentService(){
+        userInstruments=new HashMap<>();
+    }
 
     //add instrument
     abstract InstrumentDO addInstrument(InstrumentDO instrumentDO);
